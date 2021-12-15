b315ced6f03a7e809445e68b3d4bdf3a062bf320
davidvous <ledavidvu@gmail.com>
2021-12-14 16:54:59 -0800

    cart_items model

---
+++ app/models/cart_items.py
@@ -0,0 +1,21 @@
+from .db import db
+
+class Cart_item(db.Model):
+    __tablename__ = "cart_items"
+
+    id= db.Column(db.Integer, primary_key=True)
+    product_id = db.Column(db.Integer, db.ForeignKey('products.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False, default=0)
+    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+    updated_at = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'product_id': self.product_id,
+            'user_id': self.user_id,
+            'quantity': self.quantity,
+            'created_at': self.created_at,
+            'updated_at': self.updated_at,
+        }
