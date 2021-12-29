65d7de666710713f25c919efce917a3debe28ea5
davidvous <ledavidvu@gmail.com>
2021-12-28 17:01:24 -0800

    tooled review stars

--- react-app/src/components/UserReviews/UserReviews.js
+++ react-app/src/components/UserReviews/UserReviews.js
@@ -6,14 +6,29 @@ function UserReviews({reviewInfo}) {
         <div className="userReviews__user">
           {reviewInfo.user_first_name} {reviewInfo.user_last_name}
         </div>
-        <div className="userReviews_rating">{reviewInfo.rating}
-        <span>{reviewInfo.headline}</span>
+        <div className="userReviews_rating">
+          {Array(5)
+            .fill()
+            .map((_, i) => {
+              let currentRating = i + 1;
+              return (
+                <p key={i}>
+                  <i
+                    key={i}
+                    className={`fas fa-star ${
+                      currentRating <= reviewInfo.rating
+                        ? `star-yellow`
+                        : `star-gray`
+                    }`}
+                  ></i>
+                </p>
+              );
+            })}
+          <span>{reviewInfo.headline}</span>
         </div>
         <span>Reviewed on {reviewInfo.created_at}</span>
         <span>Verified Purchase</span>
-        <div className="userReviews__review">
-            {reviewInfo.body}
-        </div>
+        <div className="userReviews__review">{reviewInfo.body}</div>
       </div>
     );
 }
