87c7d3cb42090bed8e453797a7d7961c85812e5f
davidvous <ledavidvu@gmail.com>
2022-01-02 06:22:23 -0800

    changed orders valeus to be nullable

--- app/seeds/orders.py
+++ app/seeds/orders.py
@@ -2,13 +2,13 @@ from app.models import db, Order
 
 
 def seed_orders():
-    order1 = Order(user_id=1, items={1:5, 2:5, 3:5}, address="123 Demo Street", city='Somewhere', state='The World', zipCode='99999', credit_card="4444333322221111",
+    order1 = Order(user_id=1, items={1:5, 2:5, 3:5}, address="123 Demo Street", city='Somewhere', state='AL', zipCode='99999', credit_card="4444333322221111",
     expiration_date="1226", first_name="Demo", last_name="User", cc_code="123")
     order2 = Order(user_id=2, items={1:5, 2:5, 3:5}, address="7000 Las Vegas Blvd", city='Las Vegas', state='NV', zipCode='89115', credit_card="4444333322221111",
     expiration_date="1226", first_name="David", last_name="Le", cc_code="123")
     order3 = Order(user_id=3, items={1:5, 2:5, 3:5}, address="287 S Tampa Ave", city='Orlando', state='FL', zipCode='32805', credit_card="4444333322221111",
     expiration_date="1226", first_name="Malaa", last_name="Revolt", cc_code="123")
-    order4 = Order(user_id=1, items={2:2, 3:2, 5:10}, address="123 Demo Street", city='Somewhere', state='The World', zipCode='99999', credit_card="4444333322221111",
+    order4 = Order(user_id=1, items={2:2, 3:2, 5:10}, address="123 Demo Street", city='Somewhere', state='AL', zipCode='99999', credit_card="4444333322221111",
     expiration_date="1226", first_name="Demo", last_name="User", cc_code="123")
 
     db.session.add(order1)
