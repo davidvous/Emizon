99cc46273a4bdd09350c90cb27b2b491c3694acb
davidvous <ledavidvu@gmail.com>
2022-01-03 07:39:08 -0800

    added more product seeders

--- app/seeds/products.py
+++ app/seeds/products.py
@@ -11,9 +11,27 @@ def seed_products():
     product4 = Product(name='Hello Kitty Rubber Finger - Ring Grip', description="Size - (2cm x 1.2cm x 1.1cm) (0.8in x 0.5in x 0.5in)",
     price="1.50", department="Household", product_url="https://cdn.discordapp.com/attachments/920474033932349511/921362051941662740/daiso_hellokitty.jpeg")
     product5 = Product(name='Agatsuma Zenitsu Okurimono 3D Illusion Anime Lamp', description="USB charging port, easy to charge. Made of durable and beautiful acrylic glass, it can be used with LED lights, saving energy and protecting eyes. Environmentally friendly acrylic material, harmless to health and very good quality.",
-    price="26.99", department="Toy", product_url="https://cdn.discordapp.com/attachments/920474033932349511/921362007574339584/demon_slayer_light.jpg")
+    price="26.99", department="Toys", product_url="https://cdn.discordapp.com/attachments/920474033932349511/921362007574339584/demon_slayer_light.jpg")
     product6 = Product(name='Tokyo Ghoul Kaneki Ken Action Figure Awakened', description="Size: about 23 cm high, hand-made in environmental protection, will not cause any harm to the human body, it is a very worthy collection of anime models for anime fans.",
-    price="32.98", department="Toy", product_url="https://media.discordapp.net/attachments/920474033932349511/921362869352824842/kaneki_figure.jpg")
+    price="32.98", department="Toys", product_url="https://media.discordapp.net/attachments/920474033932349511/921362869352824842/kaneki_figure.jpg")
+    product7 = Product(name='Catan Board Game (Base Game)', description="STRATEGY GAME: Trade, build and settle the Island of CATAN in this addictively fun strategy game previously called Settlers of CATAN.",
+    price="37.40", department="Games", product_url="https://cdn.discordapp.com/attachments/927581171016138842/927581210602004520/catan.jpg")
+    product8 = Product(name='My Hero Academia: Collection Box Seasons 1-3 [DVD]', description="From studio BONES, the creators of Fullmetal Alchemist and Soul Eater, comes My Hero Academia, a superhero origin story that soars to the top of the class. Izuku has dreamt of being a hero all his life a lofty goal for anyone, but especially challenging for a kid with no superpowers.",
+    price="63.37", department="Movies", product_url="https://cdn.discordapp.com/attachments/927581171016138842/927581775218217001/mha.jpg")
+    product9 = Product(name='Milano Shag Ivory Bean Bag Chair, Ivory', description="DETAILS MAKE THE DIFFERENCE: Our Milano features a sturdy handle to make it a breeze to move and is double stitched with dual zippers for extra safety and durability.",
+    price="97.21", department="Furniture", product_url="https://cdn.discordapp.com/attachments/927581171016138842/927582293139292210/chair.jpg")
+    product10 = Product(name='Marukome Miso Gluten Free', description="Restaurant quality miso. All natural and gluten free-Great for vegetarians.",
+    price="97.21", department="Food", product_url="https://cdn.discordapp.com/attachments/927581171016138842/927582746040233984/miso.jpg")
+    product11 = Product(name='Jujutsu Kaisen 0', description="The prequel to the supernatural exorcist adventure Jujutsu Kaisen!",
+    price="8.99", department="Books", product_url="https://cdn.discordapp.com/attachments/927581171016138842/927583367707369542/jjk.jpg")
+    product12 = Product(name='Luwint LED Colorful Flashing Finger Lighting Gloves', description="6 Mode 3 Colors LED Gloves Rave Light Finger Lighting Flashing Glow Mittens. Easy to use. Different color settings and patterns the light run through at night by pressing the button on the wrist",
+    price="12.99", department="Electronics", product_url="https://cdn.discordapp.com/attachments/927581171016138842/927584031548268604/glowhands.png")
+    product13 = Product(name='Natures Sunshine LBS II, 100 Capsules', description="Natural Constipation Relief - Cascara sagrada bark, buckthorn bark, licorice root, and capsicum are natural laxatives for constipation commonly found in detox teas",
+    price="17.16", department="Health", product_url="https://cdn.discordapp.com/attachments/927581171016138842/927585087044878357/constipation.jpg")
+    product14 = Product(name='Leowefowa Koi Fish And Sakura Flower Canvas', description="High definition picture photo prints on canvas with vivid color using fade-resistant inks on thick high quality canvas. The finishing artwork is durable, moisture resistant and color fade-resistant.",
+    price="13.98", department="Art", product_url="https://cdn.discordapp.com/attachments/927581171016138842/927585510174625872/koifish.jpg")
+    product14 = Product(name='Exodus by Hikaru Utada', description="2004-10-05 Single Set",
+    price="12.98", department="Music", product_url="https://cdn.discordapp.com/attachments/927581171016138842/927586249861103637/utada.jpeg")
 
     db.session.add(product1)
     db.session.add(product2)
@@ -21,6 +39,15 @@ def seed_products():
     db.session.add(product4)
     db.session.add(product5)
     db.session.add(product6)
+    db.session.add(product7)
+    db.session.add(product8)
+    db.session.add(product9)
+    db.session.add(product10)
+    db.session.add(product11)
+    db.session.add(product12)
+    db.session.add(product13)
+    db.session.add(product14)
+    db.session.add(product15)
     db.session.commit()
 
 def undo_products():
--- app/seeds/reviews.py
+++ app/seeds/reviews.py
@@ -6,17 +6,17 @@ def seed_reviews():
     review2 = Review(user_id=2, product_id=1, headline="Whoever got the last one, watch out!", body="I have a very particular set of skills; skills I have acquired over a very long career. Skills that make me a nightmare for people like you.",
     rating=1)
     review3 = Review(user_id=1, product_id=2, headline="I got the last one of these too!", body="Can't catch me I'm the gingerbread man!",
-    rating=1)
+    rating=5)
     review4 = Review(user_id=3, product_id=2, headline="These are great. This is my 12th pair!", body="Feels great and looks stylish!",
-    rating=1)
+    rating=5)
     review5 = Review(user_id=4, product_id=3, headline="Would definitely get this again!", body="This was a great buy!",
-    rating=1)
+    rating=4)
     review6 = Review(user_id=1, product_id=4, headline="This is great to protect your hands!", body="When you type all day and need some protection for your fingers, this is the thing to get! I love the color too!",
-    rating=1)
+    rating=4)
     review7 = Review(user_id=1, product_id=5, headline="This light is awesome! I can't believe how cool it is!", body="Zenitsu is the best character in Demon Slayer and I love this light. It makes me feel like I can do Thunder Breathing too.",
-    rating=1)
+    rating=5)
     review8 = Review(user_id=1, product_id=6, headline="Kaneki! Or can I believe it?", body="Such a cool figurine! I have it next to my Zenitsu light and no monsters will dare come out to attack me.",
-    rating=1, reviewImg='https://media.discordapp.net/attachments/917541871457275925/918846475897798727/default-user.jpeg')
+    rating=5, reviewImg='https://media.discordapp.net/attachments/917541871457275925/918846475897798727/default-user.jpeg')
 
     db.session.add(review1)
     db.session.add(review2)
