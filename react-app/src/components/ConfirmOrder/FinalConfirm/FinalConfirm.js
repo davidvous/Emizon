8d80ef73b7a742ed72ff5819dae7b0247ac5c807
davidvous <ledavidvu@gmail.com>
2022-01-02 22:24:58 -0800

    fixed review modal

--- react-app/src/components/ConfirmOrder/FinalConfirm/FinalConfirm.js
+++ react-app/src/components/ConfirmOrder/FinalConfirm/FinalConfirm.js
@@ -7,7 +7,7 @@ function FinalConfirm() {
           <h4>An error occured with your order!</h4>
         </div>
          <div className="final__confirm__middle">
-             <h4>Please check your shipping or payment information.</h4>
+             <h4>Please review your shipping or payment information.</h4>
          </div>
          <div className="final__confirm__bottom">
          </div>
